66eb9dcb21083b583dfbe976bf354e106ab70b49
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-04-08 11:27:04 -0400

    coreccion

--- code/db-basico.php
+++ code/db-basico.php
@@ -1,8 +1,12 @@
 <?php
 
-$db_name = "registro";
+$db_name = "registro2";
 $db_user = "registro-user";
 $db_pass = "user1";
 
+try  {
 $dsn = "mysql:host=localhost;dbname=$db_name";
 $db = new PDO($dsn, $db_pass, $db_user);
+}catch(PDOException $e){
+    echo $e;
+}
