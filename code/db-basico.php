5e8229d5e0c4e8d87f4637bbb7821adf96322af8
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-04-08 11:32:24 -0400

    mejora de msj error

--- code/db-basico.php
+++ code/db-basico.php
@@ -5,8 +5,8 @@ $db_user = "registro-user";
 $db_pass = "user1";
 
 try  {
-$dsn = "mysql:host=localhost;dbname=$db_name";
-$db = new PDO($dsn, $db_pass, $db_user);
+    $dsn = "mysql:host=localhost;dbname=$db_name";
+    $db = new PDO($dsn, $db_pass, $db_user);
 }catch(PDOException $e){
-    echo $e;
+    echo $e->getMessage();
 }
