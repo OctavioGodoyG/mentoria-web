ec35fb9d55f1972b206bfc76fb75807b8bee5b4b
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-04-08 11:25:11 -0400

    envio de conección

--- code/db-basico.php
+++ code/db-basico.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "Archivo de conección a bd";
+$db_name = "registro";
+$db_user = "registro-user";
+$db_pass = "user1";
+
+$dsn = "mysql:host=localhost;dbname=$db_name";
+$db = new PDO($dsn, $db_pass, $db_user);
