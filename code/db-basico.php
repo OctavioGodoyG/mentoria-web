84962f42b8e8370adaf90e6ff5837b4669fb2996
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-04-13 15:54:04 -0400

    delete

--- code/db-basico.php
+++ code/db-basico.php
@@ -12,22 +12,28 @@ try  {
 }
 
 //preparar consulta
-$sql = "INSERT INTO users
-        (full_name, email, user_name, password)
-        VALUES
-        (:full_name, :email, :user_name, :password);";
+// $sql = "INSERT INTO users
+//         (full_name, email, user_name, password)
+//         VALUES
+//         (:full_name, :email, :user_name, :password);";
 
-//statement
-$stmt = $db->prepare($sql);
+// //statement
+// $stmt = $db->prepare($sql);
 
-$full_name = 'Juan PErez';
-$email = 'juan.perez@segic.usach.cl';
-$user_name = 'juan.perez';
-$password = 'juan123';
+// $full_name = 'Juan PErez';
+// $email = 'juan.perez@segic.usach.cl';
+// $user_name = 'juan.perez';
+// $password = 'juan123';
 
-$stmt->bindParam(':full_name',$full_name);
-$stmt->bindParam(':email',$email);
-$stmt->bindParam(':user_name',$user_name);
-$stmt->bindParam(':password',$password);
+// $stmt->bindParam(':full_name',$full_name);
+// $stmt->bindParam(':email',$email);
+// $stmt->bindParam(':user_name',$user_name);
+// $stmt->bindParam(':password',$password);
 
+// $stmt->execute();
+
+
+$id=3;
+$stmt = $db->prepare("DELETE FROM users where id=:id");
+$stmt->bindParam(':id',$id);
 $stmt->execute();
