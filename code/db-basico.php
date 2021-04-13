7edb305b532a571e8a0fe13b9a5c64d95f7cbd45
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-04-13 15:20:40 -0400

    insert a bd

--- code/db-basico.php
+++ code/db-basico.php
@@ -4,9 +4,30 @@ $db_name = "registro2";
 $db_user = "registro-user";
 $db_pass = "user1";
 
-try  {
+try  { 
     $dsn = "mysql:host=localhost;dbname=$db_name";
     $db = new PDO($dsn, $db_pass, $db_user);
 }catch(PDOException $e){
     echo $e->getMessage();
 }
+
+//preparar consulta
+$sql = "INSERT INTO users
+        (full_name, email, user_name, password)
+        VALUES
+        (:full_name, :email, :user_name, :password);";
+
+//statement
+$stmt = $db->prepare($sql);
+
+$full_name = 'Juan PErez';
+$email = 'juan.perez@segic.usach.cl';
+$user_name = 'juan.perez';
+$password = 'juan123';
+
+$stmt->bindParam(':full_name',$full_name);
+$stmt->bindParam(':email',$email);
+$stmt->bindParam(':user_name',$user_name);
+$stmt->bindParam(':password',$password);
+
+$stmt->execute();
