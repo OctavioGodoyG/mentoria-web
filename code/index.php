2d131b4c1b151cb09dcd60fe9027bd73a316d587
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-04-16 17:42:13 -0400

    correccion

--- code/index.php
+++ code/index.php
@@ -1,77 +1,77 @@
 <?php
-	//echo "Hola Mundo";
-	require "util/db.php";
+//echo "Hola Mundo";
+require "util/db.php";
 
-	if (isset($_POST["sing-up-button"])){
-		// se envio form
-		$db=connectDB();
+if (isset($_POST["sing-up-button"])) {
+	// se envio form
+	$db = connectDB();
 
-		//print_r($_POST);
+	//print_r($_POST);
 
-		$name = $_POST["name"];
-		$email= $_POST["email"];
-		$username= $_POST["username"];
-		$pass= $_POST["pass"];
-		$repeatPass= $_POST["repeat-pass"];
-		$rememberMe= $_POST["remember-me"];
-		
-		//preparar consulta
-		$sql = "INSERT INTO users
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$username = $_POST["username"];
+	$pass = $_POST["pass"];
+	$repeatPass = $_POST["repeat-pass"];
+	$rememberMe = $_POST["remember-me"];
+
+	//preparar consulta
+	$sql = "INSERT INTO users
 				(full_name, email, user_name, password)
 				VALUES
 				(:full_name, :email, :user_name, :password);";
 
-		 $stmt = $db->prepare($sql);
+	$stmt = $db->prepare($sql);
 
-		$stmt->bindParam(':full_name',$name);
-		$stmt->bindParam(':email',$email);
-		$stmt->bindParam(':user_name',$username);
-		$stmt->bindParam(':password',password_hash($pass, PASSWORD_DEFAULT));
+	$pass = password_hash($pass, PASSWORD_DEFAULT);
+	$stmt->bindParam(':full_name', $name);
+	$stmt->bindParam(':email', $email);
+	$stmt->bindParam(':user_name', $username);
+	$stmt->bindParam(':password', $pass);
 
-		$stmt->execute();
-		echo "Registro realizado";
-
-
-
-	}else{
-		echo "No se ha enviado pagina por boton";
-	}
+	$stmt->execute();
+	echo "Registro realizado";
+} else {
+	echo "No se ha enviado pagina por boton";
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Registro Mentoria Web</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body style="background-color: #999999;">
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="login100-more" style="background-image: url('images/bg-01.jpg');"></div>
@@ -88,7 +88,7 @@
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 						<span class="label-input100">Email</span>
 						<input class="input100" type="text" name="email" placeholder="Email addess...">
 						<span class="focus-input100"></span>
@@ -100,13 +100,13 @@
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<span class="label-input100">Password</span>
 						<input class="input100" type="text" name="pass" placeholder="*************">
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Repeat Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Repeat Password is required">
 						<span class="label-input100">Repeat Password</span>
 						<input class="input100" type="text" name="repeat-pass" placeholder="*************">
 						<span class="focus-input100"></span>
@@ -125,7 +125,7 @@
 							</label>
 						</div>
 
-						
+
 					</div>
 
 					<div class="container-login100-form-btn">
@@ -145,23 +145,24 @@
 			</div>
 		</div>
 	</div>
-	
-<!--===============================================================================================-->
+
+	<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/daterangepicker/moment.min.js"></script>
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
 </body>
+
 </html>
