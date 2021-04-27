afabf0f3045d30eba205a0405aadfa19e1b1be21
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-04-27 10:26:51 -0400

    send create

--- html-crud-template/create.php
+++ html-crud-template/create.php
@@ -1,6 +1,41 @@
+<?php
+require "util/db.php";
+
+if (isset($_POST['send-button'])) {
+
+    $db = connectDB();
+
+    //$idregistro = $_POST["id"];
+    $name = $_POST["full_name"];
+    $email = $_POST["email"];
+    $username = $_POST["user_name"];
+    $password = $_POST["password"];
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    //preparar consulta
+    $sql = "INSERT INTO users
+				(full_name, email, user_name, password)
+				VALUES
+				(:full_name, :email, :user_name, :password);";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(':full_name', $name);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':user_name', $username);
+    $stmt->bindParam(':password', $password);
+
+    $stmt->execute();
+
+    echo "Registro actualizado con éxito";
+}
+?>
+
+
 <!doctype html>
 <html lang="en" class="h-100">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,17 +46,18 @@
     <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
 
     <title>List of User</title>
-   
-  </head>
-  <body class="d-flex flex-column h-100">
-    
+
+</head>
+
+<body class="d-flex flex-column h-100">
+
     <div class="container pt-4 pb-4">
         <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
             <a class="navbar-brand" href="#">HTML CRUD Template</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-    
+
             <div class="collapse navbar-collapse" id="navbarsExample09">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
@@ -38,38 +74,55 @@
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-md-0">
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
                 </form>
             </div>
         </nav>
     </div>
-        
+
     <main role="main" class="flex-shrink-0">
         <div class="container">
             <h1>Create New User</h1>
-            <form action="" method="POST">
+
+            <form action="create.php" method="POST">
+
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" placeholder="Enter name">
-                    <small class="form-text text-muted">Help message here.</small>
+                    <input type="text" class="form-control" name="full_name" id="full_name" placeholder="Enter name">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+
+                <div class="form-group">
+                    <label for="name">User Name</label>
+                    <input type="text" class="form-control" name="user_name" id="User-name" placeholder="Enter User">
+                </div>
+                <div class="form-group">
+                    <label for="name">Email</label>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="Enter mail">
+                </div>
+                <div class="form-group">
+                    <label for="name">Password</label>
+                    <input type="text" class="form-control" name="password" id="pass" placeholder="Enter pass">
+                </div>
+
+                <button type="submit" class="btn btn-primary" name="send-button">Submit</button>
             </form>
+
         </div>
     </main>
-      
+
     <footer class="footer mt-auto py-3">
         <div class="container pb-5">
             <hr>
             <span class="text-muted">
-                    Copyright &copy; 2019 | <a href="https://pisyek.com">Pisyek.com</a>
+                Copyright &copy; 2019 | <a href="https://pisyek.com">Pisyek.com</a>
             </span>
         </div>
     </footer>
 
-    
+
     <script src="assets/js/jquery-3.3.1.slim.min.js"></script>
     <script src="assets/js/popper.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
-  </body>
+</body>
+
 </html>
