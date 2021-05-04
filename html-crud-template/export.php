4aad0a329d16761922d7c7c0f2ca517e231ea8c0
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-05-03 23:06:47 -0400

    se crea xport

--- html-crud-template/export.php
+++ html-crud-template/export.php
@@ -3,8 +3,6 @@ require 'vendor/autoload.php';
 require "util/db.php";
 
 $db = connectDB();
-// header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-// header('Content-Disposition: attachment; filename="lista_usuarios.xlsx"');
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
@@ -15,20 +13,15 @@ $sheet->setCellValue('A1', 'Id');
 $sheet->setCellValue('B1', 'Full Name');
 $sheet->setCellValue('C1', 'user Name');
 $sheet->setCellValue('D1', 'Email');
+$sheet->setCellValue('F1', 'Password');
 
 $sql = "SELECT * FROM users";
 $stmt = $db->prepare($sql);
 $stmt->execute();
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($users as $key => $user){
-
-    $sheet->setCellValue('A'.$key, $user['id']);
-    $sheet->setCellValue('B'.$key, $user['full_name']);
-    $sheet->setCellValue('C'.$key, $user['email']);
-    $sheet->setCellValue('D'.$key, $user['user_name']);
-
-}
+// Set details for the formula that we want to evaluate, together with any data on which it depends
+$sheet->fromArray($users, null, 'A2');
 
 $writer = new Xlsx($spreadsheet);
 $writer->save('usuarios.xlsx');
