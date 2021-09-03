cd9671856432320edefc2ec4356981b226dcc870
octavio.godoy <octavio.godoy@segic.usach.cl>
2021-09-03 15:41:14 -0400

    control de html scrpting

--- laravel/blog/resources/views/posts.blade.php
+++ laravel/blog/resources/views/posts.blade.php
@@ -19,7 +19,8 @@
                      <!-- modifica $post->slug> -->
                      <a href="/post/<?= $post->id?>">
                         <!-- < ?= $post->title ?> -->
-                        {{$post->title}}
+                        {{$post->title}} 
+                        <!-- {!!$post->title!!} evita control de  script--> 
                     </a>  
                 </h1>    
                 <p><?= $post->resumen ?></p> 
